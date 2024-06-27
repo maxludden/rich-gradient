2fdc604130c61b683a9355ecb52a02b9ad66b63e
Max Ludden <dev@maxludden.com>
2024-06-27 03:47:54 -0400

    update __init__.py

--- src/rich_gradient/__init__.py
+++ src/rich_gradient/__init__.py
@@ -5,6 +5,7 @@ from rich_gradient.color import Color
 from rich_gradient.default_styles import DEFAULT_STYLES
 from rich_gradient.gradient import Gradient
 from rich_gradient.spectrum import Spectrum
+from rich_gradient.rule import GradientRule
 from rich_gradient.theme import GRADIENT_TERMINAL_THEME, GradientTheme
 
 __all__ = [
@@ -12,6 +13,7 @@ __all__ = [
     "ColorType",
     "DEFAULT_STYLES",
     "Gradient",
+    "GradientRule",
     "GRADIENT_TERMINAL_THEME",
     "GradientTheme",
     "Spectrum",
