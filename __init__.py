fb00bab3f39c4c8505c986d3e00e99effebc42e1
Max Ludden <dev@maxludden.com>
2025-05-16 06:07:03 -0400

    Update metadata

--- __init__.py
+++ __init__.py
@@ -2,16 +2,16 @@
 from rich.console import Console
 from rich.traceback import install
 
-from rich_gradient.color import RGBA, BaseColor, ColorError, ColorType
-from rich_gradient._color import Color
+from rich_gradient._rgba import RGBA, RGBAError
+from rich_gradient.color import Color, ColorError, ColorType
 from rich_gradient.default_styles import DEFAULT_STYLES
-from rich_gradient.gradient import Gradient
+from rich_gradient.depreciated._gradient import Gradient
 from rich_gradient.rule import GradientRule
 from rich_gradient.spectrum import Spectrum
 from rich_gradient.theme import GRADIENT_TERMINAL_THEME, GradientTheme
 
 __all__ = [
-    "BaseColor",
+    "Console",
     "Color",
     "ColorError",
     "ColorType",
@@ -21,6 +21,7 @@ __all__ = [
     "GRADIENT_TERMINAL_THEME",
     "GradientTheme",
     "RGBA",
+    "RGBAError",
     "Spectrum",
 ]
 
