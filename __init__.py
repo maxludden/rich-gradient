b465cca74373532325aca68a787f61e63ccbdb1f
Maxwell Ludden <max@ludden.dev>
2025-07-31 08:48:30 -0400

    Disabled Logging

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ from rich.traceback import install as tr_install
 
 from rich_color_ext import install
 from rich_gradient.default_styles import DEFAULT_STYLES
-from rich_gradient.gradient import Gradient
+from rich_gradient._gradient import Gradient
 from rich_gradient.rule import Rule
 from rich_gradient.spectrum import Spectrum
 from rich_gradient.theme import GRADIENT_TERMINAL_THEME, GradientTheme
